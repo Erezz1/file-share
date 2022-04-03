222662bb956b49ca57471bc3f3a60d7749a1eeef
Neto Martinez <netostrikr@gmail.com>
2022-04-02 19:24:00 -0600

    url updated

--- utilities/url.js
+++ utilities/url.js
@@ -1,2 +1,2 @@
 export const SERVER_URL = 'https://file-share-erez.herokuapp.com';
-export const CLIENT_URL = 'https://file-share-lime.vercel.app';
+export const CLIENT_URL = 'https://file-share-rouge.vercel.app';
