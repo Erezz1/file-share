bc2fc7701a4af960dd51639aaaae4dc7ca71158b
Ernesto Perez <netostrikr@gmail.com>
2022-07-16 14:36:32 -0500

    Modificacion de las variables de entorno

--- utilities/url.js
+++ utilities/url.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'https://file-share-erez.herokuapp.com';
-export const CLIENT_URL = 'https://file-share-rouge.vercel.app';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8080';
+export const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000';
 
 // export const SERVER_URL = 'http://localhost:8080';
 // export const CLIENT_URL = 'http://localhost:3000';
